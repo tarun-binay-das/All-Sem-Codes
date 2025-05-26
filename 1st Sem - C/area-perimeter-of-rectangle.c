d1a99e870390703c7f8ed8739fd68ad23dc8ae60
Tarun Binay Das <tarunbinaydas@gmail.com>
2025-05-26 19:15:30 +0530

    1

--- 1st Sem - C/area-perimeter-of-rectangle.c
+++ 1st Sem - C/area-perimeter-of-rectangle.c
@@ -3,14 +3,10 @@
 int main()
 {
   float a, b, c, d;
-
   printf("Enter lenght & bredth: ");
   scanf("%f%f", &a, &b);
-
   c = a * b;
   d = 2 * (a + b);
-  
   printf("AREA is: %f\nPERIMETER is: %f\n", c, d);
-
   return 0;
 }
