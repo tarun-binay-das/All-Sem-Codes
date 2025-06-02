d6825e50b98b038d6ca1388cfcbccd9fd78bf2fe
Tarun Binay Das <tarunbinaydas@gmail.com>
2025-06-02 12:02:37 +0530

    1

---
+++ 1st Sem - C/odd-even-check.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  printf("Enter the number: ");
+  scanf("%d", &a);
+
+  if (a % 2 == 0)
+  {
+    printf("%d is EVEN NUMBER", a);
+  }
+  else
+  {
+    printf("%d is ODD NUMBER", a);
+  }
+
+  return 0;
+}
